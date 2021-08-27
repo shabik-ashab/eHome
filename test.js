db9fbc6356f3fc6992fbe23d5cd3db03a7af72e4
nub69 <shabik.ashab@gmail.com>
2021-08-28 00:12:41 +0600

    special product responsive

--- test.js
+++ test.js
@@ -1,2 +1,3 @@
-const  division=  num1, num2 => console.log(num1 /num2)
-division(4,2)
+const premikas= { name: 'keka ferdousi' , cars: { brand: 'toyota'} };
+const {brand} = premikas.cars;
+console.log(brand)
